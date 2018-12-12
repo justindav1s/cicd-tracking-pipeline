7ea531f1a7653add93dcd26ce6cf75bea58236d3
Justin Davis <justinndavis@gmail.com>
2018-12-12 09:31:04 +0000

    git igore

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@
 node('maven') {
 
     stage('Checkout Source') {
-        git url: "${git_url}", branch: 'master'
+        git url: "${git_url}", branch: 'master', credentialsId: '1c0e3c0a-f7bd-444e-918f-69799380d061'
     }
 
     dir(".") {
