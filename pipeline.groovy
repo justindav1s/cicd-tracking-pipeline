8aeccf31f1b16fe442f291a4e6aee502f2b316e8
Justin Davis <justinndavis@gmail.com>
2018-12-12 12:14:03 +0000

    git igore

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,33 +2,33 @@
 
 node('maven') {
 
-    stage('Checkout Source') {
+    dir("build")    {
         git url: "${git_url}", branch: 'master', credentialsId: '1c0e3c0a-f7bd-444e-918f-69799380d061'
+        def commitId  = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
     }
 
-    dir(".") {
+    stage('Checkout Source') {
 
-        def commitId  = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+    }
 
-        stage('loadfile') {
+    dir("build-metadata") {
+
+        stage('manage version data') {
+            git url: "${git_url}", branch: 'master', credentialsId: '1c0e3c0a-f7bd-444e-918f-69799380d061'
             def workspace = pwd()
             def versionFileName = "version"
             versionFileName = workspace+"/"+versionFileName
-            println versionFileName
+            print versionFileName
             def versiondata = sh(returnStdout: true, script: "cat ${versionFileName} | head -1")
-            println versiondata
+            println "Existing version data : "+versiondata
             def versionnumber = versiondata.tokenize(':')[0]
             def gitcommitid = versiondata.tokenize(':')[1]
-            println "versionnumber : "+versionnumber
-            println "gitcommitid : "+gitcommitid
-
             int newVersion = versionnumber.toInteger()
             newVersion = newVersion + 1
             def newVersionString = newVersion+":"+commitId
-            println "newVersionString : "+newVersionString
+            println "New version data :  : "+newVersionString
             sh(returnStdout: true, script: "echo ${newVersionString} > ${versionFileName}")
             def newversiondata = sh(returnStdout: true, script: "cat ${versionFileName} | head -1")
-            println "newversiondata : "+newversiondata
             sshagent(['1c0e3c0a-f7bd-444e-918f-69799380d061']) {
                 sh ("git config --global user.email \"justinndavis@gmail.com\"")
                 sh ("git config --global user.name \"Justin Davis\"")
@@ -36,8 +36,6 @@ node('maven') {
                 sh ("git commit -m \"updating version data to ${newVersionString}\"")
                 sh ("git push origin master")
             }
-
-            //sh(returnStdout: true, script: "git add version && git commit -m\"updating version data to ${newVersionString}\" && git push")
         }
 
     }
